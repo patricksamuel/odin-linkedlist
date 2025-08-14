0a41230e726830e1935677e34837b43caf657499
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-14 19:02:09 +0200

    allfucntions on odin done

--- src/index.js
+++ src/index.js
@@ -26,15 +26,149 @@ class LinkedList {
             while(currentNode.nextNode !== null) { //that means if current node is empty exit the while loop
                 console.log("iterate");
                 currentNode = currentNode.nextNode;
-
             }
             currentNode.nextNode = newNode;
+        }
+    }
+    prepend(nodeValue) {
+        const newNode = new node(nodeValue, null)
+        let currentheadNode = this.nodeMember;
+        this.nodeMember = newNode
+        this.nodeMember.nextNode = currentheadNode;
+    }
+    size(){
+        let headNode = this.nodeMember;
+        if (headNode == null){
+            console.log(0)
+            return 0;
+        }
+        else{
+            let currentNode = headNode;
+            let count = 1;
+            while (currentNode.nextNode !== null){
+                count++;
+                currentNode = currentNode.nextNode;
+            }
+            console.log(count)
+            return count
+        }
+    }
+    head(){
+        if (this.nodeMember == null){
+            return null;
+        }
+        else{
+            let headNodeReturn = new node(this.nodeMember.value,null)
+            console.log(headNodeReturn.value)
+            return headNodeReturn
+        }
+    }
+    tail(){
+        if (this.nodeMember == null){
+            return null;
+        }
+        else{
+            let currentNode = this.nodeMember;
+            while (currentNode.nextNode !== null){
+                currentNode = currentNode.nextNode;
+            }
+            console.log(currentNode.value)
+            return currentNode
+        }
+    }
+    at(index){
+        let currentNode = this.nodeMember;
+        for (let i = 0 ;i<index;i++){
+            currentNode = currentNode.nextNode;
+        }
+        console.log(currentNode.value);
+        return currentNode.value
+    }
+    pop(){
+        let currentNode = this.nodeMember;
+        let beforeEndingNode = null;
+        while(currentNode.nextNode !== null){
+            beforeEndingNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+        beforeEndingNode.nextNode = null
+    }
+    contains(value){
+        let currentNode = this.nodeMember;
+        while ((currentNode.nextNode !== null )&&(currentNode.value !== value)){
+            currentNode = currentNode.nextNode;
+            if(currentNode.value == value){
+                return true
+            }
+        }
+        return false
+    }
+    find(value){
+        let currentNode = this.nodeMember;
+        let i = 0;
+        if(currentNode.value === value){
+                return i
+        }
+        while ((currentNode.nextNode !== null )&&(currentNode.value !== value)){
+            currentNode = currentNode.nextNode;
+            if(currentNode.value === value){
+                return i+1
+            }
+            i++;
+        }
+        return null
+    }
+    toString(){
+        let stringOutput = "(" + this.nodeMember.value + ")"
+        let currentNode = this.nodeMember;
+        while (currentNode.nextNode !== null){
+            currentNode = currentNode.nextNode
+            stringOutput = stringOutput + "->" + "(" +currentNode.value+ ")";
+        }
+        return stringOutput
+    }
+    insertAt(value,index){
+        let currentNode = this.nodeMember
+        let newNodetoAppend = new node(value, null)
+        if (index >0){
+            for (let i = 0 ;i < index-1 ; i++){
+            currentNode = currentNode.nextNode
+            // i is incremented 
+            }
+            let temp = currentNode.nextNode;
+            currentNode.nextNode = newNodetoAppend
+            currentNode.nextNode.nextNode = temp
 
         }
-
-        console.log("value assigned");
-        console.log("recurrent empty");
+        else{
+            let currentheadNode = this.nodeMember;
+            this.nodeMember = newNodetoAppend
+            this.nodeMember.nextNode = currentheadNode;
+        }
     }
+        
+    removeAt(index){
+        let currentNode = this.nodeMember
+        let  beforeEndingNode = null;
+        if (index >0){
+            for (let i = 0 ;i < index ; i++){
+            beforeEndingNode = currentNode
+            currentNode = currentNode.nextNode
+            // i is incremented 
+            }
+            console.log("currentvalue is" + currentNode.value)
+            let temp = currentNode.nextNode;
+            currentNode = beforeEndingNode;
+            currentNode.nextNode = temp;
+            
+        }
+        else{
+            let currentheadNode = this.nodeMember.nextNode;
+            this.nodeMember = currentheadNode
+        }
+    }
+            
+
 }
 
 class node {
@@ -47,4 +181,23 @@ let newList = new LinkedList;
 newList.append("try")
 newList.append("this")
 newList.append("one")
+newList.append("two")
+newList.append("three")
+newList.prepend("now")
+newList.size()
+newList.head()
+newList.tail()
+newList.at(0)
+newList.at(2)
+//newList.pop()
+console.log(newList.contains("this"))
+console.log(newList.contains("hello"))
+console.log(newList.find("hello"))
+console.log(newList.toString())
+
+newList.insertAt("weird thing",0)
+console.log(newList.toString())
+newList.removeAt(0)
+console.log(newList.toString())
+
 console.log(newList)
